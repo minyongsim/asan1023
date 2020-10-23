d813629fc8876c7cfbf820d2d683af92a5f307c2
minyongsim <68522496+minyongsim@users.noreply.github.com>
2020-10-23 17:35:39 +0900

    ffffff

--- js/asan.js
+++ js/asan.js
@@ -1,69 +1,73 @@
 (function ($) {
-  
+
   // 로고를 클릭하면 main.html의 #content를 load() 하시오.
   $('#wrap')
-  .on("click", "#header h1 a, #footer .quickMenu a, .mainContent #step_area a, .contTit .prev a", function () {
+    .on("click", "#header h1 a, #footer .quickMenu a, .mainContent #step_area a, .contTit .prev a", function () {
       var url = this.href;
       $("#container > #content").remove();
       $("#container").load(url + " #content");
       return false;
-    }
-  );
+    });
   // `<li><div class="img"><img src="${photo}" alt=""></div>`
   // `<div class="doctorInfo"><strong>${name}</strong>`
   // `<p>${depart}</p>`
   // `<div>${about}</div></div></li>`
-$('#container ').on('click','.medicalContent .mediList a',function(e){
-  e.preventDefault()
-  var url = this.href;
-  var part = this.id;
-  $("#container > #content").remove();
-  $("#container").load(url + " #content");
 
+
+
+  var usedata;
   $.ajax({
-    type:'GET',
-    url:'data/doctors.json',
-    beforeSend:function(xhr){
-      if(xhr.overrideMimeType){
-        xhr.overrideMimeType ("application/json")
+    type: 'GET',
+    url: 'data/doctors.json',
+    beforeSend: function (xhr) {
+      if (xhr.overrideMimeType) {
+        xhr.overrideMimeType("application/json")
       }
     },
-    success:function(data){
-     var usedata = data[part]
-     var newContent ='';
-     function dataPrint() {
-       for(var i in usedata){
-         newContent += `<li><div class="img"><img src="${usedata[i].photo}" alt=""></div>`
-         newContent += `<div class="doctorInfo"><strong>${usedata[i].name}</strong>`
-         newContent += `<p>${usedata[i].depart}</p>`
-         newContent += `<div>${usedata[i].about}</div></div></li>`
-       }
-       $('#content .part1DoctorList').html(`<ul>${newContent}</ul>`)
-     }
-     dataPrint()
-    
+    success: function (data) {
+     usedata = data
+
     },
-    error:function(abc){
+    error: function (abc) {
       alert(abc.status + '오류발생')
     }
   })
-})
 
+  $('#container ').on('click', '.medicalContent .mediList a', function (e) {
+    e.preventDefault()
+    var url = this.href;
+    var part = this.id;
+    $("#container > #content").remove();
+    $("#container").load(url + " #content", function () {
+      var newContent = '';
+      for (var i in usedata[part]) {
+        newContent += `<li><div class="img"><img src="${usedata[part][i].photo}" alt=""></div>`
+        newContent += `<div class="doctorInfo"><strong>${usedata[part][i].name}</strong>`
+        newContent += `<p>${usedata[part][i].depart}</p>`
+        newContent += `<div>${usedata[part][i].about}</div></div></li>`
+      }
+      $('#content .part1DoctorList').html(`<ul>${newContent}</ul>`)
+
+    });
+
+  })
+
+ 
 
   // 헤더박스에 스크롤 이벤트 시 픽스드
-  $(window).scroll(function(){
+  $(window).scroll(function () {
     var sct = $(this).scrollTop()
     if (sct >= 50 && !$('#headeer').hasClass('on')) {
       $('#header').stop().slideUp(100).stop().slideDown(100).addClass('on')
-    }else if (sct < 50 && $('#header').hasClass('on')) {
+    } else if (sct < 50 && $('#header').hasClass('on')) {
       $('#header').removeClass('on')
-    } 
+    }
   })
-  
 
- 
 
- 
 
-    
+
+
+
+
 })(jQuery);
